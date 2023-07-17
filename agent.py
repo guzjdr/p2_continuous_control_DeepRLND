83c3b8cc22ee80fd7d92cacd51b320fe46542b36
guzjdr <guzmanr.juandavid@gmail.com>
2023-07-17 15:53:48 +0200

    Exp0: Single TD3 Agent

--- agent.py
+++ agent.py
@@ -1,36 +1,53 @@
 from common import *
 from buffer import ReplayBuffer
-from modelQ import DeepQNetwork, DuelingQNetwork
+from modelsQ import Actor, Critic
 
 
 #Module Variables
+#Replay Buffer
 BUFFER_SIZE = int(1e5) # memory replay buffer size
 BATCH_SIZE = 64 # batch size
+#Q Network Hyper-parameters
 GAMMA = 0.99 # Q learning discount size
 TAU = 1e-3 # for soft update from local network to taget network
 LR_ACTOR = 5e-4 # learning rate
-LR_CRITIC = 5e-4 # learning rate  
+LR_CRITIC = 5e-4 # learning rate
+#Update frequencies
 UPDATE_CRITIC_EVERY = 1 # number of frames used to update the local network
 UPDATE_ACTOR_TARGET = 2 * UPDATE_CRITIC_EVERY
+#Noise Parameters
+NOISE_MIN_MAX = 0.5
 
-class Agent():
-    """ Agent
-    
-    Params
-    ******
-    
+class AgentTD3():
+    """ 
+        Implementaion of TD3 Agent. 
+        Please refer to the TD3 description and implementation here:
+        https://spinningup.openai.com/en/latest/algorithms/td3.html
+        https://github.com/sfujim/TD3
     """
-    def __init__(self, state_size, action_size, seed, duelingQNet=False):
-        """ """
-        #Environment Deifiniton Variables
+    def __init__(self, state_size, action_size, seed):
+        """ Initialization of the agent class 
+        
+        Params
+        ******
+            state_size (int): Observation (state) vector size
+            action_size (int): Action vector size
+            seed (int): Random seed
+        """
+        #Environment Defined Variables
         self.state_size = state_size
         self.action_size = action_size
         self.seed = random.seed(seed)
-        
-        #Deep-Q Networks
-        self.qnet_local = DeepQNetwork(state_size, action_size, seed).to(device) if not duelingQNet else DuelingQNetwork(state_size, action_size, seed).to(device)
-        self.qnet_target = DeepQNetwork(state_size, action_size, seed).to(device) if not duelingQNet else DuelingQNetwork(state_size, action_size, seed).to(device) 
-        self.optimizer = optim.Adam(self.qnet_local.parameters(), lr=LR)
+
+        #Actor Networks
+        self.actor_local = Actor(state_size, action_size, seed).to(device)
+        self.actor_target = Actor(state_size, action_size, seed).to(device)
+        self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=LR_ACTOR)
+
+        #Critic Networks
+        self.critic_local = Critic(state_size, action_size, seed).to(device)
+        self.critic_target = Critic(state_size, action_size, seed).to(device)
+        self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr=LR_ACTOR)
         
         #Buffer - Replay memory
         self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
@@ -38,55 +55,109 @@ class Agent():
         self.t_step = 0
         
     def step(self, state, action, reward, next_state, done):
-        """ """
+        """ Collection of experiences and learning from those experiences 
+        Params
+        ******
+            state (Numpy Array): previous observed agent state
+            action (int): Action taken to change from state to next_state
+            reward (float): Reward acquired from state-action pair
+            next_state (Numpy Array): Next observed agent state
+            done (boolean): Sequence is done boolean
+        """
         #Update memory
-        #pdb.set_trace()
+            #pdb.set_trace() - For Debugging
+        self.t_step += 1
         self.memory.add(state, action, reward, next_state, done)
-        
-        #Learn every number of time steps - UPDATE_EVERY
-        self.t_step = (self.t_step + 1) % UPDATE_EVERY
-        if self.t_step == 0:
-            #
+        #Learn every number of time steps according to variable UPDATE_EVERY
+        if self.t_step % UPDATE_CRITIC_EVERY == 0:
+            #Learn from enough experiences
             if len(self.memory) > BATCH_SIZE:
                 experiences = self.memory.sample()
                 self.learn(experiences, GAMMA)
-    def act(self, state, eps=0.):
-        """ """
-        state = torch.from_numpy(state).float().unsqueeze(0).to(device)
-        self.qnet_local.eval()
-        with torch.no_grad():
-            action_values = self.qnet_local(state)
-        self.qnet_local.train()
 
-         # Epsilon-greedy action selection
-        if random.random() > eps:
-            return np.argmax(action_values.cpu().data.numpy())
+    def act(self, state, use_target=False, add_noise=False):
+        """ 
+        Compute the action based on the current state vector
+        The noise is implemented based on Mr. Fujimoto's concept of noise
+        Params
+        ******
+            state (Numpy Array): current state
+            use_target (Boolean): return target actions
+            add_noise (Boolean): add noise to the actions 
+        """
+        state = torch.from_numpy(state).float().to(device) if not torch.is_tensor(state) else state
+        self.actor_local.eval()
+        with torch.no_grad():
+            if use_target:
+                action_values = self.actor_target(state).cpu().data.numpy().astype('float64')
+            else:
+                action_values = self.actor_local(state).cpu().data.numpy().astype('float64')
+            #pdb.set_trace()
+        self.actor_local.train()
+        #pdb.set_trace()
+        # Add noise to the action vector
+        if add_noise:
+            noise = np.clip(np.random.random_sample(action_values.shape), -NOISE_MIN_MAX, NOISE_MIN_MAX)
+            action_values = np.clip(action_values + noise, -1, 1)
         else:
-            return random.choice(np.arange(self.action_size))
+            action_values = np.clip(action_values, -1, 1)
+
+        return action_values
         
     def learn(self, experiences, gamma):
-        """ """
-        states, actions, rewards, next_states, dones = experiences
+        """ 
         
-        Q_targets_next = self.qnet_target(next_states).detach().max(1)[0].unsqueeze(1)
-        #Update target net q values
-        Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
+        Params
+        ******
+            experiences (Pytorch Tensor Tuple): Experience tuple - (state, action, reward, next_state, done)
+            gamma (float): Q learning discount factor
+        """
+        states, actions, rewards, next_states, dones = experiences
+
+        # ------------------------------- Update Critic Network ---------------------------------------------- #
+        #Adding noise to the target actions
+        actions_next = self.act(next_states, use_target=True, add_noise=True)
+        actions_next = torch.from_numpy(actions_next).float().to(device)
+        # Compute Target Q Values
+        Q1_Target, Q2_Target = self.critic_target(next_states, actions_next)
+        Q_Target_next = torch.min(Q1_Target, Q2_Target)
+        Q_Targets = rewards + (gamma * Q_Target_next * (1 - dones))
         
         #Expected Q values from local network
-        Q_expected = self.qnet_local(states).gather(1, actions.long())
+        Q_expected = self.critic_local.Q1(states, actions)
         
         #Compute the loss
-        loss = F.mse_loss(Q_expected, Q_targets)
+        loss = F.mse_loss(Q_expected, Q_Targets)
         #Minimize the loss
-        self.optimizer.zero_grad()
+        #pdb.set_trace()
+        self.critic_optimizer.zero_grad()
         loss.backward()
-        self.optimizer.step()
-        
-        #Slow annealing of target network w.r.t local network
-        self.soft_update(self.qnet_local, self.qnet_target, TAU)
+        self.critic_optimizer.step()
+
+        # ------------------------------- Update Local Actor and Target Networks ------------------------------ #
+        if self.t_step % UPDATE_ACTOR_TARGET == 0:
+            #Update local actor network !
+            actions_pred = self.actor_local(states)
+            actor_loss = -self.critic_local.Q1(states, actions_pred).mean()
+            #Minimize the loss
+            self.actor_optimizer.zero_grad()
+            actor_loss.backward()
+            self.actor_optimizer.step()
+
+            #Slow annealing of target network w.r.t local network
+            self.soft_update(self.actor_local, self.actor_target, TAU)
+            #Update Frozen Target Critic Network
+            self.soft_update(self.critic_local, self.critic_target, TAU)
     
     def soft_update(self, local_model, target_model, tau):
-        """ """
+        """ Soft annealing of the target network weights w.r.t to the local network weights
+        
+        Params
+        ******
+            local_model (Pytorch Network): Local Network
+            target_model (Pytorch Network): Tarrget Network
+            tau (float): Annealing factor 
+        """
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             target_param.data.copy_(tau*local_param.data + (1.0-TAU)*target_param.data)
         
